6d8087e016cbb182af9a4ab1a1a2970450479d5a
cengineer <omerfaruk.acar@gmail.com>
2020-03-12 12:24:24 +0300

    pcaket-sniffer updated
    It parses the Ethernet header and also the UDP and ICMP headers.

--- utilities.py
+++ utilities.py
@@ -59,3 +59,8 @@ def defineTargetType (user_agent):
        return "Server"
     else:
        return "Client"
+
+#Convert a string of 6 characters of ethernet address into a dash separated hex string
+def eth_addr (a) :
+  b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
+  return b
